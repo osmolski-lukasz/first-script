0ad2221de7b5e49f35c4e01257f4b368e48d3f08
Łukasz Osmólski <93lukaszo93@gmail.com>
2017-04-20 20:26:40 +0200

    zadanie 8.4 i 8.5

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,5 @@ var a = prompt('Podaj długość boku trójkąta'),
     h = prompt('Podaj wysokość trójkąta');
 alert('Pole Twojego trójkąta wynosi ' + a*h/2);
 console.log('Pole Twojego trójkąta wynosi ' + a*h/2);
+
+//alert('Ten komunikat nie wyświetli się')
