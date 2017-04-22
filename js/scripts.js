37330c41faaffc3a4d28d9d4a21d03867e2f5ec9
Łukasz Osmólski <93lukaszo93@gmail.com>
2017-04-22 15:48:09 +0200

    Zadanie 8.6

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // plik scripts.js
 
-
+/*
 
 var a = prompt('Podaj długość boku trójkąta'),
     h = prompt('Podaj wysokość trójkąta');
@@ -8,3 +8,18 @@ alert('Pole Twojego trójkąta wynosi ' + a*h/2);
 console.log('Pole Twojego trójkąta wynosi ' + a*h/2);
 
 //alert('Ten komunikat nie wyświetli się')
+
+*/
+
+var a,
+    b,
+    value = (a * a) + (2 * a * b) - (b * b);
+console.log(value);
+
+var value = value > 0 ?
+    console.log('Wynik dodatni')
+    : console.log('Wynik ujemny');
+
+var value = value === 0 ?
+    console.log('Jest równe 0')
+    : console.log('Jest różne od zera');
